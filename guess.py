7cf9c0e06ee092b0ba4c3e6896fe44745a7e1c90
Irvin <irvinndunga01@outlook.fr>
2026-06-17 10:26:47 +0100

    Commit 6 : ajout du compteur de tentatives

--- guess.py
+++ guess.py
@@ -2,13 +2,15 @@ import random
 
 print("=== Guess the Number ===")
 nombre_secret = random.randint(1, 100)
+tentatives = 0
 
 while True:
     choix = int(input("Votre tentative : "))
+    tentatives += 1
     if choix < nombre_secret:
         print("Trop petit !")
     elif choix > nombre_secret:
         print("Trop grand !")
     else:
-        print("Bravo ! Vous avez trouve !")
+        print(f"Bravo ! Trouve en {tentatives} tentative(s) !")
         break
