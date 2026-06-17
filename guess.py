e74d60fe74d3da8476c3cc7a78f10c53d7afb99f
Irvin <irvinndunga01@outlook.fr>
2026-06-17 10:24:54 +0100

    Commit 5 : ajout d'une boucle while

--- guess.py
+++ guess.py
@@ -1,15 +1,14 @@
-# Jeu : Devinez le nombre secret
 import random
 
 print("=== Guess the Number ===")
-print("Je pense a un nombre entre 1 et 100.")
-
 nombre_secret = random.randint(1, 100)
-choix = int(input("Votre tentative : "))
 
-if choix < nombre_secret:
-    print("Trop petit !")
-elif choix > nombre_secret:
-    print("Trop grand !")
-else:
-    print("Bravo ! Vous avez trouve !")
+while True:
+    choix = int(input("Votre tentative : "))
+    if choix < nombre_secret:
+        print("Trop petit !")
+    elif choix > nombre_secret:
+        print("Trop grand !")
+    else:
+        print("Bravo ! Vous avez trouve !")
+        break
