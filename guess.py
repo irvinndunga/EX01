e0b18c77efe50be4afc4ae7f54b271990f730f80
Irvin <irvinndunga01@outlook.fr>
2026-06-17 10:07:15 +0100

    Commit 3 : ajout de commentaires

--- guess.py
+++ guess.py
@@ -1,6 +1,13 @@
+# Jeu : Devinez le nombre secret
 import random
+
+# Génération du nombre aléatoire entre 1 et 100
 nombre_secret = random.randint(1, 100)
+
+# Demande à l'utilisateur de deviner
 choix = int(input("Devinez : "))
+
+# Comparaison et affichage du résultat
 if choix < nombre_secret:
     print("Trop petit")
 elif choix > nombre_secret:
